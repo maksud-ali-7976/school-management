dfe88bbff671cc00278fd9d61bbf3fc820730d6b
maksud ali <maksudkhinchi41@gmail.com>
2024-12-03 17:16:47 +0530

    vercel add

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server:{
-    proxy:{
-      '/user':'http:localhost/5000'
-    }
-  }
-})
-
+  base: '/',
+});
