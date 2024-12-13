168c1bbc831177b43b35b122f3f2495f633b4b71
maksud ali <maksudkhinchi41@gmail.com>
2024-12-13 18:44:30 +0530

    somn changes

--- frontend/src/components/config/Data.jsx
+++ frontend/src/components/config/Data.jsx
@@ -38,64 +38,7 @@ export const  Subject = [
 ];
 
 
-export const ClassData = [
-    {
-        id:"ukg",
-        name:"UKG"
-    },
-    {
-        id:"lkg",
-        name:"LKG"
-    },
-    {
-        id:"1st",
-        name:"First Class"
-    },
-    {
-        id:"2nd",
-        name:"Second Class"
-    },
-    {
-        id:"3rd",
-        name:"Third Class"
-    },
-    {
-        id:"4th",
-        name:"Fourth Class"
-    },
-    {
-        id:"5th",
-        name:"Fifth Class"
-    },
-    {
-        id:"6th",
-        name:"Sixth Class"
-    },
-    {
-        id:"7th",
-        name:"Seventh Class"
-    },
-    {
-        id:"8th",
-        name:"Eight Class"
-    },
-    {
-        id:"9th",
-        name:"Ninth Class"
-    },
-    {
-        id:"10th",
-        name:"Tenth Class"
-    },
-    {
-        id:"11th",
-        name:"Eleventh Class"
-    },
-    {
-        id:"12th",
-        name:"Twelfth Class"
-    },
-];
+
 
 
 export const Vehicle = [
