0bf3ce064cdce3cca936f41c5ea7b88f888bcb07
maksud ali <maksudkhinchi41@gmail.com>
2024-12-14 13:23:43 +0530

    some changes

--- frontend/src/middleware/public.jsx
+++ frontend/src/middleware/public.jsx
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
-const PublicRoute = async ({ children }) => {
+const PublicRoute =  ({ children }) => {
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
   if (isAuthenticated) {
     return <Navigate to="/" replace />;
