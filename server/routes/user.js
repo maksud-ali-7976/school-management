36b5b6b799a2df0658b68735d9b7f7d2c0e300f0
maksud ali <maksudkhinchi41@gmail.com>
2024-12-14 15:05:30 +0530

    som echanges

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,13 +1,16 @@
-import express from 'express';
-const router  = express.Router();
-import {HandlerForUserRegister,HandlerForUserLogin,HandlerForCheckUserAuth,HandlerForUserLogout} from '../controllers/users.js'
-import {AuthMiddleware} from '../middleware/Auth.js'
+import express from "express";
+const router = express.Router();
+import {
+  HandlerForUserRegister,
+  HandlerForUserLogin,
+  HandlerForCheckUserAuth,
+  HandlerForUserLogout,
+} from "../controllers/users.js";
+import { AuthMiddleware } from "../middleware/Auth.js";
 
-router.post('/register',HandlerForUserRegister);
-router.post('/login',HandlerForUserLogin);
-router.get('/checkAuth',HandlerForCheckUserAuth);
-router.post('/logout',HandlerForUserLogout)
-
-
+router.post("/register", HandlerForUserRegister);
+router.post("/login", HandlerForUserLogin);
+router.get("/checkAuth", HandlerForCheckUserAuth);
+router.post("/logout", AuthMiddleware, HandlerForUserLogout);
 
 export default router;
