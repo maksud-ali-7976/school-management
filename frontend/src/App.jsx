bdbe56a86a112edbfaad980c2235f81671533687
maksud ali <maksudkhinchi41@gmail.com>
2024-12-14 12:59:50 +0530

    add Protected Route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import { CheckAuth } from "./toolkit/thunk/auth";
 import Dashboard from "./pages/Dashboard";
 import AdminLayout from "./pages/AdminLayout";
-import { useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import ProtectedRoute from "./middleware/protected";
 import Teachers from "./pages/Teachers";
 import Notfound from "./pages/Notfound";
 import Student from "./pages/Student";
@@ -12,37 +10,53 @@ import Vehicle from "./pages/Vehicle";
 import ProfilePage from "./pages/ProfilePage";
 import LoginPage from "./pages/auth/login";
 import Register from "./pages/auth/Signup";
-import { useEffect } from "react";
 
 function App() {
-  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
-
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (
-      isAuthenticated &&
-      window.location.pathname !==
-        "https://school-management-beryl-zeta.vercel.app/admin/login" &&
-      window.location.pathname !==
-        "https://school-management-beryl-zeta.vercel.app/admin/register"
-    ) {
-      dispatch(CheckAuth());
-    } else {
-      navigate("/admin/login");
-    }
-  }, []);
-
   return (
     <>
       <div className="h-screen">
         <Routes>
           <Route path="/" element={<AdminLayout />}>
-            <Route index element={<Dashboard />} />
-            <Route path="teachers" element={<Teachers />} />
-            <Route path="students" element={<Student />} />
-            <Route path="driver" element={<Vehicle />} />
-            <Route path="profile" element={<ProfilePage />} />
+            <Route
+              index
+              element={
+                <ProtectedRoute>
+                  <Dashboard />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="teachers"
+              element={
+                <ProtectedRoute>
+                  <Teachers />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="students"
+              element={
+                <ProtectedRoute>
+                  <Student />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="driver"
+              element={
+                <ProtectedRoute>
+                  <Vehicle />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="profile"
+              element={
+                <ProtectedRoute>
+                  <ProfilePage />
+                </ProtectedRoute>
+              }
+            />
           </Route>
           <Route path="/admin">
             <Route path="login" element={<LoginPage />} />
