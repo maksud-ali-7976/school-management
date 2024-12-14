98a28510e6717b8a344c260b35162ed18277e087
maksud ali <maksudkhinchi41@gmail.com>
2024-12-14 13:07:34 +0530

    some

--- frontend/src/middleware/protected.jsx
+++ frontend/src/middleware/protected.jsx
@@ -4,6 +4,7 @@ import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children }) => {
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+  console.log(isAuthenticated)
   const dispatch = useDispatch();
 
   if (isAuthenticated) {
