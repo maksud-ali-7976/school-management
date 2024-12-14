15932e063c4a554d6df8689dbdc4a1cfaef32d0a
maksud ali <maksudkhinchi41@gmail.com>
2024-12-14 13:14:48 +0530

    public route

--- frontend/src/middleware/public.jsx
+++ frontend/src/middleware/public.jsx
@@ -1,12 +1,12 @@
 import { useDispatch, useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
-const PublicRoute = async ({ Children }) => {
+const PublicRoute = async ({ children }) => {
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
   if (isAuthenticated) {
     return <Navigate to="/" replace />;
   }
 
-  return Children;
+  return children;
 };
 
 export default PublicRoute;
