48f044b2c93d1531466640b29fa5fed3dc1838a6
maksud ali <maksudkhinchi41@gmail.com>
2024-12-14 08:34:22 +0530

    fix some bug

--- frontend/src/components/admin/Table.jsx
+++ frontend/src/components/admin/Table.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Data } from "../config/ClassData";
+import {Data} from '../config/data'
 const API_URL = import.meta.env.VITE_BACKEND_API;
 const Table = ({ Data, columns }) => {
   const navigate = useNavigate();
